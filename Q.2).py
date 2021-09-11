cf32e3665ae7d9d7fb5afeb7df6aadf5c15767d8
Memon-Sabir-au26 <memonsabir30@gmail.com>
2021-09-10 23:19:52 -0400

    arithmetic operators

---
+++ Q.2).py
@@ -0,0 +1,18 @@
+val1 = 10
+val2 = 5
+
+# using the addition operator
+res = val1 + val2
+print(res)
+
+# using the subtraction operator
+res = val1 - val2
+print(res)
+
+# using the multiplication operator
+res = val1 * val2
+print(res)
+
+# using the divition operator
+res = val1 / val2
+print(res)
