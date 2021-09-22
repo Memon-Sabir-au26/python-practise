d27c911ab8478ce452c4ec6d37fdd5bf49bd16c1
Memon-Sabir-au26 <memonsabir30@gmail.com>
2021-09-22 18:37:11 -0400

    python-practice on loop

---
+++ prc15.py
@@ -0,0 +1,42 @@
+# Q1. What will be the output ?
+
+def printMax(a, b):
+    if a > b:
+        print(a, 'is maximum')
+    elif a == b:
+        print(a, 'is equal to', b)
+    else:
+        print(b, 'is maximum')
+    printMax(3, 4)
+
+# Output :- Nothing will print witout any error.
+
+# Q2. What will be the output ?
+
+x = 50
+def func(x):
+    print('x is', x)
+    x = 2
+    print('Changed local x to', x)
+func(x)
+print('x is now', x)
+
+"""
+Output :- 
+x is 50
+Changed local x to 2
+x is now 50
+"""
+
+# Q3. Write a function which will take a str as input and will return a string where vowels are removed.
+
+def vowels(a):
+    newString = a;
+    vowels = ('a','e','i','o','u')
+    for i in a:
+        if i in vowels:
+            newString = newString.replace(i,"");
+    print(newString);
+
+str = input("Enter the String : ")
+vowels(str)
