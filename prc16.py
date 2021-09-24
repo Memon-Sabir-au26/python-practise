b4f9be2c333021957b8c09c9f78ec8bbb79ac7a2
Memon-Sabir-au26 <memonsabir30@gmail.com>
2021-09-24 02:13:44 -0400

    practice of list in python

---
+++ prc16.py
@@ -0,0 +1,16 @@
+# Q1. Write a function to print length of a list.
+
+list = [ 1, 2, 3, 4, 5]
+print(len(list))
+
+# Q2. Write a function to print the reverse of a list, pass the list as a parameter.
+
+list1 = [ 1, 2, 3, 4, 5]
+print(list1)
+list1.reverse()
+print(list1)
+
+# Q3. Write a function that returns(not prints) the data-type of the last element in a list.
+
+list2 = [ 1, 2, 3, 4, 5, "abc"]
+print(type(list2[-1]))
