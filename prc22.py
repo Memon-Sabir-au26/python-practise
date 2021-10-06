1840b27cdd928314fb7fddbeee7d05deaec1bf1c
Memon-Sabir-au26 <memonsabir30@gmail.com>
2021-10-06 21:01:10 +0530

    python loop practise

--- prc22.py
+++ prc22.py
@@ -7,3 +7,29 @@ for i in range(n,0,-1):
     for j in range(1,i+1):
         print("*",end=" ")
     print()
+
+n=5
+for k in range(1,5):
+    for l in range(1,k+1):
+        print("*",end=" ")
+    print()
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+num = int(input("enter the number of row : "))
+for i in range(0,num):
+    for j in range(0,num-i-1):
+        print(end=" ")
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print()
+
+num = int(input("enter the number of row : "))
+for i in range(num+1,0,-1):
+    for j in range(0,num-i+1):
+        print(end=" ")
+    for j in range(0,i-1):
+        print("*",end=" ")
+    print()
